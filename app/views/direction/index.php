d2c42d54565d846d71c5230c332264c9e312e15a
YoPTA <s.nichipurenko@yandex.ru>
2017-07-28 15:31:43 +0300

    Table view test
    Тестируется таблица с классом view

--- app/views/direction/index.php
+++ app/views/direction/index.php
@@ -6,11 +6,34 @@ $page_id = 'page_moderator';
 //Подключаем шапку
 include APP_VIEWS . 'layouts/header.php';
 ?>
-    <div class="uk-width-1-2" align="left">
+
         <h1><?= $pagetitle ?></h1>
+        <div class="uk-width-8-10" align="left">
+            <table class="view">
+                <caption align="bottom"><?= $total_direction; ?></caption>
+                <tr>
+                    <th>№</th>
+                    <th>Название</th>
+                    <th>Состояние</th>
+                </tr>
+
+                <tr class="srow">
+                    <td>1</td>
+                    <td>Какое-то направление</td>
+                    <td>Включен</td>
+                </tr>
+                <tr class="srow">
+                    <td>2</td>
+                    <td>Какое-то направление</td>
+                    <td>Выключен</td>
+                </tr>
+
+            </table>
+
+        </div>
+
         <div class="uk-width-1-2"><?php print_r($directions) ?></div>
-        <div class="uk-width-1-2"><?php echo $total_direction; ?></div>
-    </div>
+        <div class="uk-width-1-2"><?= $total_direction; ?></div>
 
 
 
