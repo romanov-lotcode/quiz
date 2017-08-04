0afed18f3c23637a72643fd6480ec8c86de9693e
YoPTA <s.nichipurenko@yandex.ru>
2017-08-04 15:31:26 +0300

    Directions to view
    Добавлен вывод направлений

--- app/controllers/DirectionController.php
+++ app/controllers/DirectionController.php
@@ -9,9 +9,9 @@ class DirectionController extends BaseController
         $is_can = false;
         $search = [];
         $page = 1;
+        $index_number = 1;
         $directions = [];
         $total_direction = 0;
-
 
         foreach ($user_right as $u_r)
         {
@@ -22,12 +22,36 @@ class DirectionController extends BaseController
             }
         }
 
+        if (isset($_GET['page']))
+        {
+            $page = htmlspecialchars($_GET['page']);
+        }
+
+        if ($page < 1)
+        {
+            $page = 1;
+        }
+
+        $html_element['name'] = new \HTMLElement\HTMLTextStringElement();
+        $html_element['name']->setName('s_name');
+        $html_element['name']->setId('s_name');
+        $html_element['name']->setCaption('Направление');
+        $html_element['name']->setConfig('type', 'text');
+        $html_element['name']->setConfig('class', 'uk-width-1-1');
+        $html_element['name']->setConfig('placeholder', 'Направление');
+        $html_element['name']->setValueFromRequest();
+
+        if ($html_element['name']->getValue() != null)
+        {
+            $search['name'] = trim($html_element['name']->getValue());
+        }
+
         if ($is_can)
         {
-            //$page = 2;
-            //$search['name'] = 'нап';
             $directions = Direction::getDirections($search, $page);
             $total_direction = Direction::getTotalDirections($search);
+            $index_number = ($page - 1) * Direction::SHOW_BY_DEFAULT;
+            $pagination = new Pagination($total_direction, $page, Direction::SHOW_BY_DEFAULT, 'page=');
 
             include_once APP_VIEWS.'direction/index.php';
         }
--- app/views/direction/index.php
+++ app/views/direction/index.php
@@ -9,8 +9,22 @@ include APP_VIEWS . 'layouts/header.php';
 
         <h1><?= $pagetitle ?></h1>
         <div class="uk-width-8-10" align="left">
-            <table class="uk-width-1-1 view">
 
+            <form method="POST" class="uk-form simple">
+                <table class="uk-width-1-1 search_param">
+                    <tr>
+                        <td class="uk-width-1-3">
+                            <?= $html_element['name']->render() ?>
+                        </td>
+                        <td class="uk-width-1-3">
+                            <button class="uk-button" name="registration">Поиск</button>
+                        </td>
+                        <td class="uk-width-1-3"></td>
+                    </tr>
+                </table>
+            </form>
+
+            <table class="uk-width-1-1 view">
                 <tr>
                     <th class="uk-width-1-10">№</th>
                     <th class="uk-width-6-10">Название</th>
@@ -18,32 +32,39 @@ include APP_VIEWS . 'layouts/header.php';
                     <th class="uk-width-2-10">Действие</th>
                 </tr>
 
-                <tr class="srow">
-                    <td>1</td>
-                    <td>Какое-то направление</td>
-                    <td>Включен</td>
-                    <td></td>
-                </tr>
-                <tr class="srow">
-                    <td>2</td>
-                    <td>Какое-то направление</td>
-                    <td>Выключен</td>
-                    <td></td>
-                </tr>
+                <?php
+                if (is_array($directions) && count($directions) > 0):
+                    $i=0;
+                    foreach ($directions as $d_item):
+                        $index_number++;
+                        $i++;
+                ?>
+
+                        <tr class="srow">
+                            <td><?= $index_number ?></td>
+                            <td><?= $d_item['name'] ?></td>
+                            <td><?= $d_item['flag'] ?></td>
+                            <td>Кнопки</td>
+                        </tr>
+                <?php
+                    endforeach; //foreach ($directions as $d_item):
+                endif; //if (is_array($directions) && count($directions) > 0):
+                ?>
 
                 <?php
                 include APP_VIEWS . 'layouts/record_count.php';
-                echo recordCount($total_direction, 2);
+                echo recordCount($total_direction, $i);
                 ?>
 
                 <!-- <caption align="bottom"><?//= $total_direction; ?></caption> -->
 
             </table>
 
+            <div class="uk-width-1-1"><?php print_r($directions) ?></div>
         </div>
 
-        <div class="uk-width-1-2"><?php print_r($directions) ?></div>
-        <div class="uk-width-1-2"><?= $total_direction; ?></div>
+
+
 
 
 
