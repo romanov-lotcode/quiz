be2a93d3e28cc87f24cdf18b99f89e17b030dfec
YoPTA <s.nichipurenko@yandex.ru>
2017-08-08 16:17:19 +0300

    App_State changed
    Изменились состояния:
    0 - Невозможно изменить
    1 - Вкл
    2 - Выкл

--- components/App_State.php
+++ components/App_State.php
@@ -13,13 +13,17 @@ class App_State
     public function getFlagState($value)
     {
         $result = 'Не определено';
+        if ($value == 2)
+        {
+            $result = 'Выкл <span class="uk-icon-close" style="color: red;" title="Выключено"></span>';
+        }
         if ($value == 1)
         {
             $result = 'Вкл <span class="uk-icon-check" style="color: green;" title="Включено"></span>';
         }
         if ($value == 0)
         {
-            $result = 'Выкл <span class="uk-icon-close" style="color: red;" title="Выключено"></span>';
+            $result = 'Н.и. <span class="uk-icon-check" style="color: green;" title="Включено (Невозможно изменить)"></span>';
         }
         return $result;
     }
