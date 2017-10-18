e21343f2cb374b01915d57f72c4c8a3b6a4f6f72
YoPTA <s.nichipurenko@yandex.ru>
2017-10-18 15:27:59 +0300

    Revert "New message_type in app"
    This reverts commit e61bcf246f7c152a41f519abb92eaa7036af4947.

--- components/App_Message.php
+++ components/App_Message.php
@@ -3,12 +3,6 @@
 
 class App_Message
 {
-    /**
-     * Получить сообщение
-     * @param string $message - Сообщение
-     * @param string $type - Тип выводимого сообщения
-     * @return string
-     */
     public static function getMessage($message = '', $type = MESSAGE_TYPE_ERROR)
     {
         if ($type == MESSAGE_TYPE_ERROR)
@@ -17,15 +11,6 @@ class App_Message
             <div class="app_message_type_error uk-alert uk-alert-warning" data-uk-alert>
                 <a href="" class="uk-alert-close uk-close"></a>
                 <p><i class="app_message_type_error uk-icon-exclamation-triangle"></i> '. $message .'</p>
-            </div>
-            ';
-        }
-        if ($type == MESSAGE_TYPE_SUCCESS)
-        {
-            return '
-            <div class="app_message_type_success uk-alert uk-alert-success" data-uk-alert>
-                <a href="" class="uk-alert-close uk-close"></a>
-                <p><i class="app_message_type_success uk-icon-check"></i> '. $message .'</p>
             </div>
             ';
         }
