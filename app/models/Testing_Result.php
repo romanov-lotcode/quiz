a6c776a0963d628301f0923d6ba00df91da3e050
Sergey Nichipurenko <s.nichipurenko@yandex.ru>
2018-01-08 21:46:50 +0300

    Class Testing_Result has new method
    В класс Testing_Result был добавлен новый метод getTestingResult - для
    предоставления информации о результате

--- app/models/Testing_Result.php
+++ app/models/Testing_Result.php
@@ -15,6 +15,49 @@ class Testing_Result
      *****************************************************/
 
     /**
+     * Возвращает информацию о результате тестирования
+     * @param int $id - ID
+     * @param int $user_id - ID пользователя
+     * @return bool|mixed
+     */
+    public static function getTestingResult($id, $user_id)
+    {
+        $sql = 'SELECT
+          testing_result.begin_datetime,
+          testing_result.end_datetime,
+          testing.name AS testing_name,
+          user.lastname,
+          user.firstname,
+          user.middlename,
+          testing.question_count,
+          testing.minimum_score,
+          direction.name
+        FROM
+          testing_result
+          INNER JOIN testing ON (testing_result.testing_id = testing.id)
+          INNER JOIN user ON (testing_result.user_id = user.id)
+          INNER JOIN test ON (testing.test_id = test.id)
+          INNER JOIN direction ON (test.direction_id = direction.id)
+        WHERE
+          testing_result.id = :id AND
+          testing_result.user_id = :user_id AND
+          testing_result.flag = 1';
+        $db = Database::getConnection();
+        $result = $db->prepare($sql);
+        $result->bindParam(':id', $id, PDO::PARAM_INT);
+        $result->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $result->execute();
+
+        // Обращаемся к записи
+        $testing_result = $result->fetch(PDO::FETCH_ASSOC);
+
+        if ($testing_result) {
+            return $testing_result;
+        }
+        return false;
+    }
+
+    /**
      * Получить общее количество записей, удовлетворяющих параметрам поиска
      * @param [] $search - Параметры поиска
      * @return int
