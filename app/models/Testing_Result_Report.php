99e8e7ff119118c438ea66211692c6d1fa12f2a1
Sergey Nichipurenko <s.nichipurenko@yandex.ru>
2018-01-09 22:29:35 +0300

    Class Testing_Result_Report has new method
    В класс Testing_Result_Report добавлен новый метод - для получения
    отчета результатов тестирования по результату тестирования

--- app/models/Testing_Result_Report.php
+++ app/models/Testing_Result_Report.php
@@ -15,6 +15,41 @@ class Testing_Result_Report
      *****************************************************/
 
     /**
+     * Возращает отчет результата тестирования по результату тестирования
+     * @param int $testing_result_id - ID результата тестирования
+     * @return array
+     */
+    public static function getTestingResultReportByTestingResult($testing_result_id)
+    {
+        $sql = 'SELECT
+              question.name AS question_name,
+              question.question_type_id,
+              question.explanation AS question_explanation,
+              question.path_img AS question_path_img,
+              testing_result_report.question_id,
+              testing_result_report.answer_id,
+              testing_result_report.question_time
+            FROM
+              testing_result_report
+              INNER JOIN question ON (testing_result_report.question_id = question.id)
+            WHERE
+              testing_result_report.testing_result_id = :testing_result_id';
+        $db = Database::getConnection();
+        $result = $db->prepare($sql);
+        $result->bindParam(':testing_result_id', $testing_result_id, PDO::PARAM_INT);
+        $result->execute();
+        // Получение и возврат результатов
+        $testing_result_report_list = [];
+        $i = 0;
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $testing_result_report_list[$i] = $row;
+            $i++;
+        }
+        return $testing_result_report_list;
+    }
+
+
+    /**
      * Добавляет новую запись
      * @param [] $testing_result_report - Массив с данными
      * @return bool|string
